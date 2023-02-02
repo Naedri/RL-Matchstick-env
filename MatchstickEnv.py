cd15a517b042b7ba5e0b9617bd082fe5a48329be
Adrien JALLAIS <56276737+Naedri@users.noreply.github.com>
2023-02-02 19:25:06 +0100

    doc: MatchstickEnv

--- MatchstickEnv.py
+++ MatchstickEnv.py
@@ -1,3 +1,20 @@
+# Classe MatchstickEnv représentant l'environnement d'un jeu d'allumettes. 
+
+# La méthode reset initialise l'état du jeu à num_matches.
+# La méthode reset retourne cet état. 
+
+# La méthode step permet de prendre une action, qui consiste à enlever 1, 2 ou 3 allumettes.
+# La méthode step retourne :
+# 1. l'état suivant, 
+# 2. la récompense (1 si le jeu est terminé, 0 sinon), 
+# 3. un indicateur de terminaison 
+# 4. un dictionnaire vide. 
+
+# La méthode _check_win vérifie si un joueur a gagné.
+
+# La classe hérite de gym.Env et définit les espaces d'observation et d'action en utilisant les classes spaces.Discrete de gym. 
+# Les erreurs sont également gérées en utilisant les méthodes standard de gym.Env
+
 import gym
 from gym import spaces
 
